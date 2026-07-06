5c97405447438300e856c4765eb5ea22a9e1f215
Florian <florian@iphone-26.home>
2026-07-06 20:16:20 +0200

    init

---
+++ main.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import fastify from 'fastify';
+import path from 'path';
+
+const text = fs.readFileSync('./words.csv', { encoding: 'ascii' });
+const words = text.replaceAll('\n', '').split(',');
+const app = fastify({ logger: true });
+const publicDir = path.join(__dirname, 'public');
+
+console.log(`Found ${words.length} words in words.csv file\n`);
+
+app.get('/', (req, res) => {
+    const html = fs.readFileSync(`${publicDir}/index.html`, { encoding: 'utf-8' });
+    res.type('text/html').send(html);
+});
+
+app.get('/word', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * words.length);
+    res.send(words[randomNumber].replaceAll('"', ''));
+});
+
+app.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+});
