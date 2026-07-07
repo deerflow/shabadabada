9d8d01134e74f4310aaf6e09b2f5bb2903023c63
Florian <florian@iphone-26.home>
2026-07-07 09:54:20 +0200

    added more meaningful words, reworked how words are fetched

---
+++ formatWords.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+
+const sentencesPath = path.join(__dirname, 'words.csv');
+
+const text: string = fs.readFileSync(sentencesPath, { encoding: 'utf8' });
+const sentences = text
+    .replaceAll('\n', '')
+    .replaceAll('" ', '"')
+    .replaceAll(' "', '"')
+    .split(',')
+    .map(sentence => sentence.replaceAll(',', ''))
+    .map(sentence => sentence.toLowerCase());
+
+const finalWordList = [...new Set(sentences)];
+
+fs.writeFileSync(sentencesPath, finalWordList.join(',\n'));
+
+console.log(`Formatted word list, ${finalWordList.length} entries`);
--- main.ts
+++ main.ts
@@ -3,20 +3,22 @@ import fastify from 'fastify';
 import path from 'path';
 
 const text = fs.readFileSync('./words.csv', { encoding: 'ascii' });
-const words = text.replaceAll('\n', '').split(',');
+const sentences = text
+    .replaceAll('\n', '')
+    .split(',')
+    .map(sentence => sentence.replaceAll('"', ''));
 const app = fastify({ logger: true });
 const htmlDir = path.join(__dirname, 'index.html');
 
-console.log(`Found ${words.length} words in words.csv file\n`);
+console.log(`Found ${sentences.length} words/sentences in words.csv file\n`);
 
 app.get('/', (req, res) => {
     const html = fs.readFileSync(htmlDir, { encoding: 'utf-8' });
     res.type('text/html').send(html);
 });
 
-app.get('/word', (req, res) => {
-    const randomNumber = Math.floor(Math.random() * words.length);
-    res.send(words[randomNumber].replaceAll('"', ''));
+app.get('/sentences', (req, res) => {
+    res.type('application/json').send(sentences);
 });
 
 app.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
